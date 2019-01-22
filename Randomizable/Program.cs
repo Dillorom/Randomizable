0e4a445afdd34d8927e56d89a00f32b22d8dda1d
Dillorom <37942024+Dillorom@users.noreply.github.com>
2019-01-21 20:09:26 -0800

    Add project files.

---
+++ Randomizable/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomizable
+{
+    interface IRandomizable
+    {
+        double GetRandomNum(double input);
+    }
+
+    public class MyRandomizer : IRandomizable
+    {
+        public MyRandomizer() { }
+
+        public double GetRandomNum(double input)
+        {
+            Random random = new Random();
+            double seed = random.NextDouble();
+            return seed * input;
+        }
+
+    }
+
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+
+            MyRandomizer mr = new MyRandomizer();
+
+            string str;
+            do
+            {
+                Console.WriteLine("Enter the upperbound number: ");
+                str = Console.ReadLine();
+                try
+                {
+                    double input = Double.Parse(str);
+                    Console.WriteLine($"Double number between 0 and {input}: " + mr.GetRandomNum(input), input);
+                }
+                catch (Exception e) { }
+            }
+            while (str != "exit");
+            
+            Console.ReadKey();
+            
+        }
+    }
+}
